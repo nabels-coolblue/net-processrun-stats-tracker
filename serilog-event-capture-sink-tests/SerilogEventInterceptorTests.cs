118c78a6b4741e33fd6836223930db6a34300e01
Niels Abels <n.abels@coolblue.nl>
2017-06-26 15:30:32 +0200

    Adds tests for SerilogEventInterceptor.

--- serilog-event-capture-sink-tests/SerilogEventInterceptorTests.cs
+++ serilog-event-capture-sink-tests/SerilogEventInterceptorTests.cs
@@ -3,6 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using serilog_event_capture_sink;
 using Ploeh.AutoFixture.AutoMoq;
 using Ploeh.AutoFixture;
+using Moq;
+using Serilog.Events;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace serilog_event_capture_sink_tests
 {
@@ -10,10 +14,38 @@ namespace serilog_event_capture_sink_tests
     public class SerilogEventInterceptorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LogEventLevel_OfError_ShouldRegisterAnError()
         {
-            IFixture fixture = new Fixture().Customize(new AutoConfiguredMoqCustomization()); 
+            IFixture fixture = new Fixture().Customize(new AutoConfiguredMoqCustomization());
 
+            var trackerMock = fixture.Freeze<Mock<IProcessRunStatisticsTracker>>();
+            var sut = fixture.Create<SerilogEventInterceptor>();
+            var logEvent = CreateLogEvent(LogEventLevel.Error);
+
+            sut.Intercept(logEvent);
+
+            trackerMock.Verify(m => m.RegisterError(logEvent.RenderMessage(null)), Times.Once());
+        }
+
+
+        [TestMethod]
+        public void LogEventLevel_OfFatal_ShouldRegisterAnError()
+        {
+            IFixture fixture = new Fixture().Customize(new AutoConfiguredMoqCustomization());
+
+            var trackerMock = fixture.Freeze<Mock<IProcessRunStatisticsTracker>>();
+            var sut = fixture.Create<SerilogEventInterceptor>();
+            var logEvent = CreateLogEvent(LogEventLevel.Fatal);
+
+            sut.Intercept(logEvent);
+
+            trackerMock.Verify(m => m.RegisterError(logEvent.RenderMessage(null)), Times.Once());
+        }
+
+        private LogEvent CreateLogEvent(LogEventLevel level)
+        {
+            var logEvent = new LogEvent(new DateTimeOffset(), level, null, MessageTemplate.Empty, Enumerable.Empty<LogEventProperty>());
+            return logEvent;
         }
     }
 }
