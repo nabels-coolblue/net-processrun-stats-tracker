cc0e3035a1415a904982a321c4370254c3d34aee
Niels Abels <n.abels@coolblue.nl>
2017-06-26 10:58:50 +0200

    Tracks number of errors/fatals in the application.

--- serilog-event-capture-sink/Program.cs
+++ serilog-event-capture-sink/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reactive.Linq;
+using Serilog.Events;
 
 namespace serilog_event_capture_sink
 {
@@ -12,18 +13,49 @@ namespace serilog_event_capture_sink
     {
         static void Main(string[] args)
         {
+            var processIntegrityChecker = new ProcessRunStatisticsTracker();
+
             var log = new LoggerConfiguration()
                 .WriteTo.ColoredConsole()
                 .WriteTo.Observers(events => events
                     .Do(evt => {
-                        Console.WriteLine($"Observed event {evt}");
+                        processIntegrityChecker.InterceptLogEvent(evt);
                     })
                     .Subscribe())
                 .CreateLogger();
 
             log.Information("Hello world!");
+            log.Information("Hello again world!");
+            log.Error("I'm not getting a reply :(");
+            log.Fatal("I'm outta here!");
+            log.Information("World says hello!");
+
+            var results = processIntegrityChecker.GetResults();
+
+            log.Information($"Process ran with {results.ErrorCount} errors.");
 
             Console.ReadKey();
         }
+    }
+
+    public class ProcessRunStatisticsTracker
+    {
+        public ProcessRunStatistics GetResults()
+        {
+            var statistics = new ProcessRunStatistics() { ErrorCount = _logEvents.Count(logEvent => logEvent.Level >= LogEventLevel.Error) };
+            return statistics;
+        }
+
+        private List<LogEvent> _logEvents { get; set; } = new List<LogEvent>();
+
+        public void InterceptLogEvent(LogEvent logEvent)
+        {
+            _logEvents.Add(logEvent);
+        }
+    }
+
+    public class ProcessRunStatistics
+    {
+        public int ErrorCount { get; set; }
     }
 }
